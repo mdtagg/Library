790d8397c47cf0ca4db41f129475ba4d7c49a0f0
Mike Tagg <mdtagg91@gmail.com>
2022-10-24 17:42:31 -0700

    Working solution to book card generation problems

--- index.js
+++ index.js
@@ -1,60 +1,4 @@
 
-// let myBooks = []
-
-// function book(title,author,totalPages,completedPages) {
-//     this.title = title,
-//     this.author = author,
-//     this.totalPages = totalPages,
-//     this.completedPages = completedPages
-// }
-
-// function addBook(book) {
-//     myBooks.push(book)
-// }
-
-// function displayBooks(myBooks) {
-//     for(let i = 0;i < myBooks.length;i++) {
-//         let bookContent = document.querySelector('.content')
-//         let bookCard = document.createElement('div')
-//         bookCard.classList.add('card')
-
-//         bookCard.appendChild(getBookInfo(myBooks[i]))
-//         // bookCard.textContent = `${myBooks[i]['title']}, ${myBooks[i]['author']}`
-//         bookContent.appendChild(bookCard)
-//     }
-// }
-
-// function getBookInfo(book) {
-    
-//     let info = document.createElement('pre')
-
-//     info.textContent = 
-//     `Title: ${book['title']},
-//     Author: ${book['author']},
-//     Total Pages: ${book['totalPages']},
-//     Pages Read: ${book['completedPages']}`
-    
-//     return info
-// }
-
-
-// const harryPotter = new book('Sorcerers Stone','JK Rowling','300', '250')
-
-// addBook(harryPotter)
-
-// const gameOfThrones = new book('Ice and Fire','George RR Martin', "600",'400')
-
-// addBook(gameOfThrones)
-
-// const theRoad = new book('The Road','Cormac Mccarthy','200','100')
-
-// addBook(theRoad)
-
-// console.log(myBooks)
-
-// displayBooks(myBooks)
-
-
 const addBookButton = document.querySelector('.newBookButton')
 let form = document.querySelector('.book-form')
 addBookButton.addEventListener('click', popUp)
@@ -71,33 +15,26 @@ function closeForm() {
 }
 
 
-
-
-
-
-
 //Working solution
 
 let bookCards = document.getElementById('book-card-container')
-const bookTitle = document.getElementById('title')
-const author = document.getElementById('author')
-const pages = document.getElementById('pages')
+const bookNode = document.getElementById('title')
+const authorNode = document.getElementById('author')
+const pagesNode = document.getElementById('pages')
 
 
 const addBook = document.getElementById('addBookButton')
 addBook.addEventListener('click', closeForm)
 addBook.addEventListener('click', createBookCard)
-// addBook.addEventListener('click', createBook)
 
 function createBookCard() {
     let card = document.createElement('div')
     card.classList.add('card')
     bookCards.appendChild(card)
-    console.log(author.value)
 
-    let bookTitleValue = bookTitle.value
-    let authorTitle = author.value
-    let pagesTitle = pages.value 
+    let bookTitleValue = bookNode.value
+    let authorTitle = authorNode.value
+    let pagesTitle = pagesNode.value 
 
     let newBook = new Book(bookTitleValue,authorTitle,pagesTitle)
     
@@ -113,12 +50,6 @@ function createBookCard() {
     card.appendChild(cardTitle)
     card.appendChild(authorValue)
     card.appendChild(pagesValue)
-
-
-    // card.appendChild(cardTitle)
-    // let authorValue = author.value
-    // let pagesValue = pages.value
-
 }
 
 function Book(title,author,pages) {
@@ -126,63 +57,3 @@ function Book(title,author,pages) {
     this.author = author,
     this.pages = pages
 }
-
-
-
-
-
-
-
-
-// function createBook() {
-//     let bookCard = document.getElementsByClassName('card')
-//     new book(bookTitle.value)
-    
-// }
-
-// function book(title) {
-//     this.title = title
-//     console.log(title)
-// }
-
-// bookCards.appendChild(newBook)
-
-
-
-
-
-
-// const author = document.getElementById('author')
-
-
-// let cardTitle = document.createElement('div')
-//     let cardAuthor = document.createElement('div')
-//     let cardPages = document.createElement('div')
-
-
-
-
-
-
-
-
-
-
-
-//problem
- 
-// const resultsList = document.getElementById('books')
-
-// addBook.addEventListener('click', () => popUpBookCard)
-
-// function popUpBookCard() {
-//     console.log('pop up book ran')
-//     resultsList.style.display = 'block'
-//     new URLSearchParams(window.location.search).forEach((value,name) => {
-//         resultsList.append(`${name}:${value}`)
-//         resultsList.append(document.createElement('br'))
-//     })
-// }
-
-
-// console.log(resultsList)
