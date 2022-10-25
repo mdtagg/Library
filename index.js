3f66a29f235b1988ca5f9e942735e54d0e3e90f7
Mike Tagg <mdtagg91@gmail.com>
2022-10-25 10:45:44 -0700

    checkbox added to book cards

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 
+// pop up the form menu
+
 const addBookButton = document.querySelector('.newBookButton')
 let form = document.querySelector('.book-form')
 addBookButton.addEventListener('click', popUp)
 
 function popUp() {
     form.style.display = 'block'
+    titleNode.value = ''
+    authorNode.value = ''
+    pagesNode.value = ''
 }
 
+//close button on form
 const closeButton = document.getElementById('close-button')
 closeButton.addEventListener('click', closeForm)
 
-function closeForm() {
+function closeForm(e) {
     form.style.display = 'none'
+    if(e.target.id !== 'addBookButton') {
+        titleNode.value = ''
+        authorNode.value = ''
+        pagesNode.value = ''
+    }
 }
 
-
-//Working solution
+//creating book cards
 
 let bookCards = document.getElementById('book-card-container')
-const bookNode = document.getElementById('title')
+const titleNode = document.getElementById('title')
 const authorNode = document.getElementById('author')
 const pagesNode = document.getElementById('pages')
+const haveReadNode = document.getElementById('read')
 
 
 const addBook = document.getElementById('addBookButton')
@@ -32,13 +43,15 @@ function createBookCard() {
     card.classList.add('card')
     bookCards.appendChild(card)
 
-    let bookTitleValue = bookNode.value
+    let titleValue = titleNode.value
     let authorTitle = authorNode.value
     let pagesTitle = pagesNode.value 
+    let readTitle = haveReadNode.value
 
-    let newBook = new Book(bookTitleValue,authorTitle,pagesTitle)
-    
+    let newBook = new Book(titleValue,authorTitle,pagesTitle,readTitle)
+
     let cardTitle = document.createElement('p')
+    cardTitle.classList.add('card-title')
     cardTitle.textContent = newBook.title
 
     let authorValue = document.createElement('p')
@@ -47,9 +60,16 @@ function createBookCard() {
     let pagesValue = document.createElement('p')
     pagesValue.textContent = newBook.pages
 
+    let haveReadLabel = document.createElement('label')
+
+
+    let readValue = document.createElement('input')
+    readValue.setAttribute('type','checkbox')
+
     card.appendChild(cardTitle)
     card.appendChild(authorValue)
     card.appendChild(pagesValue)
+    card.appendChild(readValue)
 }
 
 function Book(title,author,pages) {
